9fec9f3f7320f366d05b17b23a2e77cbfa07d880
zhouzhoubuchila <honghai1019@gmail.com>
2026-08-30 02:55:58 +0800

    feat: activate Family route

--- frontend/src/home-os/app/HomeOsRoutes.jsx
+++ frontend/src/home-os/app/HomeOsRoutes.jsx
@@ -4,6 +4,7 @@ import Config from '../../pages/config';
 import LegacyDashboard from '../../pages/home';
 import HomePage from '../pages/HomePage';
 import EnvironmentPage from '../pages/EnvironmentPage';
+import FamilyPage from '../pages/FamilyPage';
 import PlaceholderPage from '../pages/PlaceholderPage';
 
 export default function HomeOsRoutes({ sidebarVisible, setSidebarVisible }) {
@@ -11,7 +12,7 @@ export default function HomeOsRoutes({ sidebarVisible, setSidebarVisible }) {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/environment" element={<EnvironmentPage />} />
-      <Route path="/family" element={<PlaceholderPage type="family" />} />
+      <Route path="/family" element={<FamilyPage />} />
       <Route path="/energy" element={<PlaceholderPage type="energy" />} />
       <Route path="/homelab" element={<PlaceholderPage type="homelab" />} />
       <Route path="/legacy" element={<LegacyDashboard sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} />} />
