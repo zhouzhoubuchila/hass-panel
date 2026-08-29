7856f1b251cca7870c887c574d65423e83411002
zhouzhoubuchila <honghai1019@gmail.com>
2026-08-30 04:21:55 +0800

    feat: route dynamic attention center

--- frontend/src/home-os/app/HomeOsRoutes.jsx
+++ frontend/src/home-os/app/HomeOsRoutes.jsx
@@ -10,6 +10,7 @@ const HomelabPage = lazy(() => import('../pages/HomelabPage'));
 const EnergyPage = lazy(() => import('../pages/EnergyPage'));
 const FloorplanSettingsPage = lazy(() => import('../pages/FloorplanSettingsPage'));
 const HomeModeSettingsPage = lazy(() => import('../pages/HomeModeSettingsPage'));
+const AttentionCenterPage = lazy(() => import('../pages/AttentionCenterPage'));
 
 export default function HomeOsRoutes({ sidebarVisible, setSidebarVisible }) {
   return (
@@ -21,6 +22,7 @@ export default function HomeOsRoutes({ sidebarVisible, setSidebarVisible }) {
       <Route path="/homelab" element={<HomelabPage />} />
       <Route path="/floorplan-settings" element={<FloorplanSettingsPage />} />
       <Route path="/mode-settings" element={<HomeModeSettingsPage />} />
+      <Route path="/attention" element={<AttentionCenterPage />} />
       <Route path="/legacy" element={<LegacyDashboard sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} />} />
       <Route path="/config" element={<Config sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} />} />
       <Route path="*" element={<Navigate to="/" replace />} />
