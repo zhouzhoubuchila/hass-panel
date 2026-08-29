c5364fa34ea156b3b5e9d4af8ee1fad3f9b3abc7
zhouzhoubuchila <honghai1019@gmail.com>
2026-08-30 03:10:43 +0800

    perf: lazy load Home OS feature routes

--- frontend/src/home-os/app/HomeOsRoutes.jsx
+++ frontend/src/home-os/app/HomeOsRoutes.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Config from '../../pages/config';
-import LegacyDashboard from '../../pages/home';
 import HomePage from '../pages/HomePage';
-import EnvironmentPage from '../pages/EnvironmentPage';
-import FamilyPage from '../pages/FamilyPage';
-import HomelabPage from '../pages/HomelabPage';
-import PlaceholderPage from '../pages/PlaceholderPage';
+import RouteLoading from './RouteLoading';
+const Config = lazy(() => import('../../pages/config'));
+const LegacyDashboard = lazy(() => import('../../pages/home'));
+const EnvironmentPage = lazy(() => import('../pages/EnvironmentPage'));
+const FamilyPage = lazy(() => import('../pages/FamilyPage'));
+const HomelabPage = lazy(() => import('../pages/HomelabPage'));
+const EnergyPage = lazy(() => import('../pages/EnergyPage'));
 
 export default function HomeOsRoutes({ sidebarVisible, setSidebarVisible }) {
   return (
-    <Routes>
+    <Suspense fallback={<RouteLoading />}><Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/environment" element={<EnvironmentPage />} />
       <Route path="/family" element={<FamilyPage />} />
-      <Route path="/energy" element={<PlaceholderPage type="energy" />} />
+      <Route path="/energy" element={<EnergyPage />} />
       <Route path="/homelab" element={<HomelabPage />} />
       <Route path="/legacy" element={<LegacyDashboard sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} />} />
       <Route path="/config" element={<Config sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} />} />
       <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    </Routes></Suspense>
   );
 }
 
